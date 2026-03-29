222164a026b1e0cde60114de612f87a24dc04f38
SameerBalhara <sameerbalhara03@gmail.com>
2026-03-28 21:00:26 -0400

    hb

--- Assets/Scripts/TerrainGeneration/MeshGeneration/ChunkRingLODPolicy.cs
+++ Assets/Scripts/TerrainGeneration/MeshGeneration/ChunkRingLODPolicy.cs
@@ -8,9 +8,6 @@ public static class ChunkRingLODPolicy
         int dz = Mathf.Abs(viewer.z - target.z);
         int ring = Mathf.Max(dx, dz);
 
-        //return between 3 and 5 (5 is upper bound, 2^5)
-        //return 0;
-        //0123 to 2334
         if (ring <= 1) return 0;
         if (ring <= 3) return 1;
         if (ring <= 5) return 2;
