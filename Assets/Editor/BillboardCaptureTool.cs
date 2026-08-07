542d4695da0f5db81c901bf8e96a7beb16f7c0c2
SameerBalhara <sameerbalhara03@gmail.com>
2026-08-07 14:53:37 -0400

    spruce billboards

--- Assets/Editor/BillboardCaptureTool.cs
+++ Assets/Editor/BillboardCaptureTool.cs
@@ -18,7 +18,7 @@ public static class BillboardCaptureTool
         }
 
         int size = 1024;
-        string outputPath = "Assets/Textures/TreeBillboard.png";
+        string outputPath = "Assets/Textures/TreeBillboardSpruce.png";
 
         RenderTexture previousTarget = camera.targetTexture;
         RenderTexture previousActive = RenderTexture.active;
